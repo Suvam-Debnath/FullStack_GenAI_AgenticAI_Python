2b2d89f5c80f674d943de42a9520d52eba48ced3
Suvam Debnath <debnathsuvam322@gmail.com>
2026-04-15 17:58:56 +0530

    integrated Ollama local model with FastAPI and python APIs

--- ollama_fastapi/server.py
+++ ollama_fastapi/server.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from urllib import response
+
+from fastapi import FastAPI,Body
+from ollama import Client
 
 app = FastAPI()
+client = Client(
+    host="http://localhost:11434",   
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/contact-us")
+def read_root():
+    return {"email": "suvam@gmail.com"}
+
+@app.post("/chat")
+def chat(
+        message: str = Body(..., description="The message to send to the model")
+):    
+    response = client.chat(
+        model="gemma:2b",
+        messages=[{"role": "user", "content": message}],
+    )
+
+    return {"response": response.message.content}
