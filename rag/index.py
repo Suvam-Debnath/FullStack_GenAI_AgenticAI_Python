b4b18450be6f3d6411137592b4c4ebedf8c9598e
Suvam Debnath <debnathsuvam322@gmail.com>
2026-04-16 22:57:16 +0530

    implemented LangChain vector embeddings and stored into vector database (qdrant)

--- rag/index.py
+++ rag/index.py
@@ -1,10 +1,17 @@
+from dotenv import load_dotenv
+
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
 
+load_dotenv()
+
+# Define the path to the PDF file
 pdf_path = Path(__file__).parent / "springboot.pdf"
 
-#Load this file in python program
+# Load this file in python program
 loader = PyPDFLoader(file_path=pdf_path)
 docs = loader.load()
 
@@ -14,3 +21,18 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_overlap=400
 )
 chunks = text_splitter.split_documents(documents=docs)
+
+#Vector embeddings for the chunks
+embedding_model = OpenAIEmbeddings(
+    model="text-embedding-3-large"
+)
+
+#Create a vector store and add the chunks to it
+vector_store = QdrantVectorStore.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    url="http://localhost:6333",
+    collection_name="springboot_docs"
+)
+
+print("Document loaded, split into chunks, and added to the vector store successfully!")
